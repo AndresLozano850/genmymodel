1a9123601b9370fc07cfd9eedb841f2b2dc35c85
tomcat <tomcat@ip-172-31-19-198.eu-west-1.compute.internal>
2020-09-01 21:59:29 +0000

    Commit all changes from GenMyModel / Architecture Factory

---
+++ Volley.sql
@@ -0,0 +1,44 @@
+-- 
+-- Uncomment me if you want :)
+-- CREATE DATABASE Volley;
+
+CREATE TABLE empleado
+(
+    nombre_club String NOT NULL,
+    identificacion Integer NOT NULL,
+    cargo String NOT NULL
+);
+
+CREATE TABLE club_tenis
+(
+    nombre_club String NOT NULL,
+    telefono String NOT NULL,
+    tarifa_anual Integer NOT NULL
+);
+
+CREATE TABLE miembro_club
+(
+    id_membresia Integer NOT NULL,
+    fecha_membresia String NOT NULL,
+    tarifa Integer NOT NULL,
+    tarjeta_membresia Integer NOT NULL
+);
+
+CREATE TABLE carta
+(
+    cod_correo_entrante Integer NOT NULL,
+    fecha_verificacion String NOT NULL,
+    nuevo_miembro String NOT NULL,
+    fecha_admision String NOT NULL
+);
+
+CREATE TABLE persona
+(
+    nombre String NOT NULL,
+    apellido String NOT NULL,
+    fecha_nac String NOT NULL,
+    sexo String NOT NULL,
+    telefono Integer NOT NULL,
+    direccion String NOT NULL
+);
+
